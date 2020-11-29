1fdfa7740dfdcd738a4be7921abdb00bdf34a376
baldo-vela <baldo.vela+github@gmail.com>
2020-11-29 10:08:07 -0600

    commit tree ea0020ae34b4c97e759db348cd40171c771ea704

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,25 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+    #Change the class definition so that it inherits from the User class. Run the test suite and notice that you are passing some tests for the Student class, even without writing any code inside that class. That is because it will inherit the #first_name and #last_name methods from the User class you told it to inherit from.
+    
+    #Individual students should initialize with an instance variable, @knowledge, that points to an empty array.
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string_of_knowledge)
+        ##Define a method, #learn, that takes in a string and adds that string to the student's @knowledge array.
+        @knowledge << string_of_knowledge
+    end
+    
+    def knowledge
+        #Define a method, #knowledge, that returns that student's knowledge array.
+        return @knowledge
+    end
+    
+    
 
 end
